60aea2de2313a165e1ef8e5beb498df9db771cf8
vipin-sharma-8815 <vs3626085@gmail.com>
2025-12-18 07:53:01 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,13 @@
+
+// number => 2 to powere 54
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+console.log(typeof "vipin")     // string
+console.log(typeof undefined) // undefined
+console.log(typeof null)    // object
+console.log(typeof true) // boolean
